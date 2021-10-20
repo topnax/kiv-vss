a451468456c0d4ac6d1c15892168eed06e118ccf
Stanislav Král <stanislav.kral3@gmail.com>
2021-10-20 07:59:50 +0200

    Replaced map function with list comprehension.

--- hw/random_number_generator/stat_utils.py
+++ hw/random_number_generator/stat_utils.py
@@ -3,17 +3,17 @@ import math
 
 def expected_value(histogram):
     total = sum(histogram.values())
-    return sum(map(lambda item: (item[0] / total) * item[1], histogram.items()))
+    return sum([value * frequency / total for value, frequency in histogram.items()])
 
 
 def variance(histogram):
     ev = expected_value(histogram)
     total = sum(histogram.values())
-    return sum([((v - ev)**2) * (p / total) for v, p in histogram.items()])
+    return sum([((value - ev) ** 2) * (frequency / total) for value, frequency in histogram.items()])
 
 
 def standard_deviation(histogram):
-    return math.sqrt(expected_value({k**2: v for (k, v) in histogram.items()}) - expected_value(histogram) ** 2)
+    return math.sqrt(expected_value({k ** 2: v for (k, v) in histogram.items()}) - expected_value(histogram) ** 2)
 
 
 def test_stat_utils():
@@ -25,7 +25,6 @@ def test_stat_utils():
     }
     assert expected_value(h) == 1.350
     assert variance(h) == 1.0275
-    print(standard_deviation(h))
     assert math.isclose(standard_deviation(h), 1.0136567466)
 
 
