df63b68049f0f3b24727e08f7ebc34ac18848ff5
Stanislav Král <stanislav.kral3@gmail.com>
2021-12-13 14:45:37 +0100

    Discrete simulation - added comments

--- hw/discrete_sim/main.py
+++ hw/discrete_sim/main.py
@@ -99,11 +99,12 @@ class Patient:
             # print how many days the patient was waiting
             standard_care_waiting_time = env.now - standard_care_waiting_start
             if standard_care_waiting_time > 0:
-                self.log(env, "waited for standard care", standard_care_waiting_time, "days, but eventually made it")
-            self.log(env, "received standard care,", "beds left:", hospital.standard_beds.capacity - hospital.standard_beds.count)
+                self.log(env, f"waited {standard_care_waiting_time} days for standard care")
+            self.log(env, f"hospitalized at standard care for {days} days")
 
             # simulate n days at the standard care bed
             while day < days:
+                # one day at standard care
                 yield env.timeout(1)
                 hospital.monitor()
 
@@ -118,50 +119,51 @@ class Patient:
                     self.log(env, "died at standard care" ) 
                     patient_outcomes.append((PatientHospitalizationResult.DEATH_STD_CARE, self, env.now))
                     return 
-
                 
                 # if the patient did not die he may require to be transfered to a intensive care bed
                 if not self.transfer_to_intensive_care_required:
                     self.transfer_to_intensive_care_required = random.random() < hospital.standard_care_not_sufficient_chance
                     self.days_without_intensive_care = 0
                     
-                elif self.transfer_to_intensive_care_required:
+                if self.transfer_to_intensive_care_required:
                     # check whether there are any intensive care beds available
                     if hospital.intensive_care_beds.count >= hospital.intensive_care_beds.capacity:
+                        # intensive care bed not available
                         self.days_without_intensive_care += 1
-                        # check whether the patient requiring intensive care has been too many days on a standard care
+                        # check whether the patient requiring intensive care has been too many days on a standard care requiring intensive care
                         if self.days_without_intensive_care > self.limit_of_days_without_intensive_care:
-                            self.log(env, "died after waiting for intensive care for too long")
+                            self.log(env, "died after waiting for intensive care at standard care for too long")
                             patient_outcomes.append((PatientHospitalizationResult.DEATH_WITHOUT_INTENSIVE_CARE, self, env.now))
                             return 
                     else:
+                        # intensive care bed available
                         self.transfer_to_intensive_care_required = False
-                        self.log(env, "required int care, transfered after", self.days_without_intensive_care)
+                        self.log(env, "required int care, transfered after", self.days_without_intensive_care, "days of waiting")
                         day = days
                         transferred_to_int_care = True
                         break
                 
+        # start a new process if the patient is to be transfered to int. care
         if transferred_to_int_care:
             env.process(self.go_to_intensive_care(hospital, env, patient_outcomes))
             return
 
-
         # the patient has recovered
-        self.log(env, "recovered from standard")
+        self.log(env, "recovered at standard care")
         patient_outcomes.append((PatientHospitalizationResult.RECOVERED, self, env.now))
 
     def go_to_intensive_care(self, hospital, env, patient_outcomes):
-
         # random hospitalization days
         days = int(max(np.random.exponential(hospital.mean_intensive_care_treatment), 0))
         day = 0
 
-        self.log(env, "hospitalized for", days, "days")
+        self.log(env, "hospitalized at intensive care for", days, "days")
             
         with hospital.intensive_care_beds.request() as request:
             # get a bed
             yield request
             while day < days:
+                # one day at intensive care
                 yield env.timeout(1)
                 hospital.monitor()
                 day += 1
@@ -170,30 +172,36 @@ class Patient:
                 died = random.random() < hospital.intensive_care_death_chance
                 
                 if died:
+                    self.log(env, "died at intensive care")
                     patient_outcomes.append((PatientHospitalizationResult.DEATH_INT_CARE, self, env.now)) 
                     return
 
+            # patient recovered from intensive care
+
             # move to a standard bed
-            # standard beds are full
             if hospital.standard_beds.count >= hospital.standard_beds.capacity:
+                # standard beds are full
 
-                # number of days to recover using the standard treatment
+                # number of days to recover using the standard care treatment
                 days = max(random.gauss(hospital.mean_standard_treatment, hospital.std_dev_standard_treatment), 0)
-
+                
                 while day < days:
+                    # one day at intensive care (while requiring only standard care)
                     yield env.timeout(1)
                     hospital.monitor()
-
-
                     day += 1
                     self.total_days_at_intensive_care_2 += 1
-                    # standard beds are not full
+
+                    # standard beds are not full anymore
                     if hospital.standard_beds.count < hospital.standard_beds.capacity:
-                        self.log(env, "moved to intensive care after waiting")
+                            
+                        self.log(env, f"moved to standard care after recovering from intensive care, waited {self.total_days_at_intensive_care_2} days")
+
+                        # start a new process at standard care
                         env.process(self.go_to_standard_care(hospital, env, patient_outcomes, days - day))
                         return
                     else:
-                         # the patient may have died during the day at a standard care bed
+                         # the patient may have died during the day at the intensive care bed (requiring only standard care)
                         died = random.random() < hospital.standard_care_death_chance
                         if died:
                             self.log(env, "died in intensive care requiring standard care")
@@ -204,6 +212,7 @@ class Patient:
                 return
    
             else:
+                # recovered from intensive, moved back to standard
                 self.log(env, "recovered from intensive, moved to standard")
                 hospital.monitor()
                 env.process(self.go_to_standard_care(hospital, env, patient_outcomes))
@@ -211,20 +220,19 @@ class Patient:
         assert(false)
 
     def log(self, env, *message):
-        print(f"[{env.now}]", f"#{self.patient_id} (@{self.day_hospitalized})",*message)
+        print(f"[{env.now}]", f"#{self.patient_id} (@{self.day_hospitalized})", *message)
 
 
-def run_hospital(env, hospital, patient_outcomes):
-    standard_care_bed_used = []
-    intensive_care_bed_used = []
-    pi = 0
-    for i in range(30):
-        for j in range(np.random.poisson(3)):
-            p = Patient(pi, 4, 10, i)
-            pi += 1
+def run_hospital(env, hospital, patient_outcomes, days_with_new_patients, mean_new_patients_each_day, max_days_without_std_care, max_days_without_intensive_care):
+    patient_id = 0
+        
+    # simulate days that new patients come for health care
+    for i in range(days_with_new_patients):
+        for j in range(np.random.poisson(mean_new_patients_each_day)):
+            p = Patient(patient_id, max_days_without_intensive_care, max_days_without_std_care, i)
+            patient_id += 1
             env.process(p.go_to_standard_care(hospital, env, patient_outcomes))
-        standard_care_bed_used.append(hospital.standard_beds.count) 
-        intensive_care_bed_used.append(hospital.intensive_care_beds.count) 
+        # next day  of the simulation
         yield env.timeout(1)
         hospital.monitor()
 
@@ -247,16 +255,30 @@ def main():
     )
 
     # run the simulation
-    env.process(run_hospital(env, hospital, patient_outcomes))
+    env.process(run_hospital(env, hospital, patient_outcomes, 
+                days_with_new_patients=30, 
+                mean_new_patients_each_day=3, 
+                max_days_without_std_care=10, 
+                max_days_without_intensive_care=4
+        )
+    )
+    print("Hospital events:")
+    print("[{day}]", "#{patient_id} (@{day_of_first_hospitalization}) {message}")
     env.run()
 
+    print()
+    print("Patient hospitalization outcomes:")
+    print("#{patient_id}", "@{day_of_first_hospitalization}: {outcome} at {day}")
     # print patient results
     for (result, patient, day) in patient_outcomes:
         print(f"#{patient.patient_id}", f"@{patient.day_hospitalized}:", result, "at", day)
 
+    print()
+    print("Hospital statistics:")
+    print("@{day_s}, standard beds: {standard_beds_usage}/{standard_beds_capacity}, intensive care beds: {intensive_care_beds_usage}/{intensive_care_beds_capacity}") 
     # print day statistics
     for (day_s, s), (day_i, i) in zip(hospital.standard_beds.data.items(), hospital.intensive_care_beds.data.items()):
-        print("day:", day_s, "standard:", s, "intensive:", i)
+        print(f"@{day_s}, standard beds: {s}/{hospital.standard_beds.capacity}, intensive care beds: {i}/{hospital.intensive_care_beds.capacity}")
 
 
 if __name__ == "__main__":
